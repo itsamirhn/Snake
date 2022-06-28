b6cf10320c5c742e92ea911f55f0a07ac3c76610
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-06-28 18:08:04 +0430

    Save & Load config on file

--- src/main/java/controller/Config.java
+++ src/main/java/controller/Config.java
@@ -1,29 +1,50 @@
 package controller;
 
+import com.moandjiezana.toml.Toml;
+import com.moandjiezana.toml.TomlWriter;
+
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Config {
 
-//    private static final String FILE_PATH = "~/.snake/config.json";
-    private static final Config instance = new Config();
+    private static final String FILE_PATH = "./config.toml";
+    private static final Config instance = new Config(FILE_PATH);
 
-    public final int foodSpeed = 100;
+    public int snakeSpeed = 80;
+    public int foodSpeed = 100;
 
-    public final Dimension boardDimension = new Dimension(20, 20);
-    public final Dimension cellDimension = new Dimension(20, 20);
+    public Dimension boardDimension = new Dimension(20, 20);
+    public Dimension cellDimension = new Dimension(20, 20);
 
-    public final int snakeSpeed = 80;
 
-    private Config() {
-        loadConfig();
+    public Config() { }
+    public Config(String filePath) {
+        loadConfig(filePath);
+        try {
+            saveConfig(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
-    private void loadConfig() {
-        // TODO
+    private void loadConfig(String filePath) {
+        File file = new File(filePath);
+        if (!file.exists()) { return; }
+
+        Toml toml = new Toml().read(file);
+        Config config = toml.to(Config.class);
+        this.snakeSpeed = config.snakeSpeed;
+        this.foodSpeed = config.foodSpeed;
+        this.boardDimension = config.boardDimension;
+        this.cellDimension = config.cellDimension;
     }
 
-    private void saveConfig() {
-        // TODO
+    private void saveConfig(String filePath) throws IOException {
+        File file = new File(filePath);
+        new TomlWriter().write(this, file);
     }
 
     public static Config getInstance() {
