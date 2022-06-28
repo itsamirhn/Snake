2015f92b7f0e488c4a7e65f483537489c1706dbb
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-06-28 16:31:23 +0430

    Revert "Merge branch 'menu'"
    This reverts commit e5eba2e0f74410013871fc4e3df62b7498d77cc7, reversing
    changes made to ea026ad7f47ba35a1f7c72e4c57815e69bda1611.

--- src/view/SView.java
+++ src/view/SView.java
@@ -10,8 +10,6 @@ public class SView extends JFrame {
     public SView(SModel model) {
         super("Snake");
 
-        setJMenuBar(createMenuBar());
-
         boardPanel = new BoardPanel(model.getBoard());
         add(boardPanel);
 
@@ -36,23 +34,5 @@ public class SView extends JFrame {
     public void showGameOverDialog(String message) {
         // TODO
         System.err.println(message);
-    }
-
-    private JMenuBar createMenuBar() {
-        JMenuBar menuBar = new JMenuBar();
-        JMenu config = new JMenu("Config");
-
-        JMenu level = new JMenu("Level");
-        JMenuItem slow = new JMenuItem("Slow");
-        JMenuItem medium = new JMenuItem("Medium");
-        JMenuItem fast = new JMenuItem("Fast");
-
-        menuBar.add(config);
-        config.add(level);
-        level.add(slow);
-        level.add(medium);
-        level.add(fast);
-
-        return menuBar;
     }
 }
