a9c5f34fdbe61d348fd1c71f4495236e5453a1eb
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-07-01 23:39:24 +0430

    BonusFood radius based on age

--- src/main/java/model/BonusFood.java
+++ src/main/java/model/BonusFood.java
@@ -3,7 +3,9 @@ package model;
 import controller.Config;
 
 public class BonusFood extends Food {
-    private int timer = Config.getInstance().bonusFoodMoveTimer;
+
+    public static final int MAX_TIMER = Config.getInstance().bonusFoodMoveTimer;
+    private int timer = MAX_TIMER;
     public void tiktok() {
         if (isRemoved()) return;
         timer--;
@@ -13,6 +15,10 @@ public class BonusFood extends Food {
     @Override
     public int getScore() {
         return 3;
+    }
+
+    public float getAgeRatio() {
+        return (float) timer / MAX_TIMER;
     }
     public BonusFood(Cell container){
         super(container);
--- src/main/java/view/ElementGraphics.java
+++ src/main/java/view/ElementGraphics.java
@@ -12,31 +12,33 @@ public abstract class ElementGraphics {
 
     static int BODY_STROKE = 5;
     static int BODY_ARC_RADIUS = 10;
-    static int FOOD_RADIUS = 10;
 
     public static void draw(JComponent container, Graphics2D g2d, Element element) {
-        if(element instanceof BonusFood){
-            drawBonusFood(container, g2d);
-        } else if (element instanceof Food) {
-            drawFood(container, g2d);
-        } else if (element instanceof Body) {
-            drawBody(container, g2d);
+        if(element instanceof BonusFood bonusFood){
+            drawBonusFood(container, g2d, bonusFood);
+        } else if (element instanceof Food food) {
+            drawFood(container, g2d, food);
+        } else if (element instanceof Body body) {
+            drawBody(container, g2d, body);
         }
     }
 
-    private static void drawBody(JComponent container, Graphics2D g2d) {
+    private static void drawBody(JComponent container, Graphics2D g2d, Body body) {
         g2d.setColor(Color.GREEN);
         g2d.setStroke(new BasicStroke(BODY_STROKE));
         g2d.drawRoundRect(BODY_STROKE, BODY_STROKE, container.getWidth() - 2 * BODY_STROKE, container.getHeight()- 2 * BODY_STROKE, BODY_ARC_RADIUS, BODY_ARC_RADIUS);
     }
 
-    private static void drawFood(JComponent container, Graphics2D g2d) {
+    private static void drawFood(JComponent container, Graphics2D g2d, Food food) {
         g2d.setColor(Color.RED);
-        g2d.fillOval(container.getWidth() / 2 - FOOD_RADIUS, container.getHeight() / 2 - FOOD_RADIUS, 2 * FOOD_RADIUS, 2 * FOOD_RADIUS);
+        int radius = Math.min(container.getWidth(), container.getHeight()) / 2;
+        g2d.fillOval(container.getWidth() / 2 - radius, container.getHeight() / 2 - radius, 2 * radius, 2 * radius);
     }
-    private static void drawBonusFood(JComponent container, Graphics2D g2d) {
+    private static void drawBonusFood(JComponent container, Graphics2D g2d, BonusFood bonusFood) {
         g2d.setColor(Color.YELLOW);
-        g2d.fillOval(container.getWidth() / 2 - FOOD_RADIUS, container.getHeight() / 2 - FOOD_RADIUS, 2 * FOOD_RADIUS, 2 * FOOD_RADIUS);
+        int radius = Math.min(container.getWidth(), container.getHeight()) / 2;
+        int currentRadius = (int) (bonusFood.getAgeRatio() * radius);
+        g2d.fillOval(container.getWidth() / 2 - currentRadius, container.getHeight() / 2 - currentRadius, 2 * currentRadius, 2 * currentRadius);
     }
 
 }
